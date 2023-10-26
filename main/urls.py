661182056d434dd92cdd1fd6793886f1386c256d
SEBASTIAN <sebsion8@gmail.com>
2023-10-26 15:44:19 +0000

    supported living page complete

--- main/urls.py
+++ main/urls.py
@@ -3,12 +3,12 @@ from main import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("our-location/", views.location, name="location"),
+    path("about us/our-location/", views.location, name="location"),
     path("contact-us/", views.contact_us, name="contact_us"),
-    path("who-we-are/", views.who_we_are, name="who_we_are"),
-    path("why-choose-living-plus-care/", views.why_choose_us, name="why_choose_us"),
-    path("Our corporate social responsibility/", views.corporate_responsibility, name="corporate_responsibility"),
-    path("Domicialiary care service/", views.domicialiary_service, name="domicialiary_service"),
-    path("Supported living service/", views.supported_service, name="supported_service"),
-    path("Location/", views.location, name="location"),
+    path("about-us/who-we-are/", views.who_we_are, name="who_we_are"),
+    path("about-us/why-choose-living-plus-care/", views.why_choose_us, name="why_choose_us"),
+    path("about-us/our-corporate-social-responsibility/", views.corporate_responsibility, name="corporate_responsibility"),
+    path("services/domicialiary-care/", views.domicialiary_service, name="domicialiary_service"),
+    path("services/supported-living/", views.supported_service, name="supported_service"),
+    path("locations/", views.location, name="location"),
 ]
