c5b93cb36c2baf53ed3534238779bfd3c33dc54a
SEBASTIAN <sebsion8@gmail.com>
2023-10-27 23:20:21 +0000

    deleted asssets dir

--- plc/settings.py
+++ plc/settings.py
@@ -140,7 +140,7 @@ EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "asssets")
+STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
